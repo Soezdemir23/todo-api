44507456f89aca409f4bb8fd2fd9c9a856e3f3be
Serdar Özdemir <soezdemir3423@gmail.com>
2023-04-01 12:37:51 +0200

    refactoring and implementation of login,register
    1. register function has now additional feature implemented:
    + check if username exists in database
    + return an error
    
    2. loginUser has been implemented
    + checks for username in database
    + returns vague error if not found
    + returns vague error if password doesn't match
    + creates a payload with Jwt
    + returns them to the clientside
    + throws an InternalServerError if all else fails

--- src/auth/auth.service.ts
+++ src/auth/auth.service.ts
@@ -1,4 +1,8 @@
-import { Injectable, InternalServerErrorException } from '@nestjs/common';
+import {
+  Injectable,
+  InternalServerErrorException,
+  UnauthorizedException,
+} from '@nestjs/common';
 import { InjectRepository } from '@nestjs/typeorm';
 import { UserEntity } from 'src/User/Entity/user.entity';
 import { Repository } from 'typeorm';
@@ -7,10 +11,12 @@ import { RegisteredUserDTO } from 'src/User/dto/user.dto';
 import { UserLoginDTO } from './dto/userLogin.dto';
 
 import * as cr from 'crypto';
+import { JwtService } from '@nestjs/jwt';
 @Injectable()
 export class AuthService {
   constructor(
     @InjectRepository(UserEntity) private userRepo: Repository<UserEntity>,
+    private jwt: JwtService,
   ) {}
 
   async registerUser(registeredUserDTO: RegisteredUserDTO) {
@@ -21,22 +27,50 @@ export class AuthService {
       hashLength: 40,
       salt: cr.randomBytes(32),
     });
-
     const user = new UserEntity();
     user.username = username;
     user.passwordHashedSalted = hashed;
 
+    // check if the username is already taken and throw an error if it is, else save the user
     try {
-      return await this.userRepo.save(user);
+      const userExists = await this.userRepo.findOne({ where: { username } });
+      if (userExists) {
+        throw new InternalServerErrorException(
+          'User already exists. Please choose a different username',
+        );
+      }
+      await this.userRepo.save(user);
     } catch (error) {
       throw new InternalServerErrorException(
-        "Something went wrong, user wasn't created!",
+        'Something went wrong while registering the user',
       );
     }
   }
-
+  // try to be vague about the error message to prevent brute force attacks
   async loginUser(userLoginDTO: UserLoginDTO) {
-    console.log(userLoginDTO);
-    return userLoginDTO;
+    const { username, password } = userLoginDTO;
+    try {
+      const user = await this.userRepo.findOne({ where: { username } });
+      if (!user) {
+        throw new UnauthorizedException('Invalid credentials!');
+      }
+
+      const isMatch = await ag2.verify(user.passwordHashedSalted, password);
+      // always the same with the ambigious !isMatch. Never really works when you need it to.
+      if (isMatch === false) {
+        throw new UnauthorizedException('Invalid credentials!');
+      }
+      const jwtPayload = { username };
+      const jwtToken = await this.jwt.signAsync(jwtPayload, {
+        expiresIn: '1d',
+        algorithm: 'HS512',
+      });
+      // this returns the token to the user, I pasted it into jwt.io and it works!
+      return { token: jwtToken };
+    } catch (error) {
+      throw new InternalServerErrorException(
+        'Something went wrong while logging in',
+      );
+    }
   }
 }
