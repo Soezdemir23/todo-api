be79e75f08adf8d5ce5b5fcc0dff19333164fa18
Serdar Özdemir <soezdemir3423@gmail.com>
2023-04-07 12:43:43 +0200

    Move footer specific css rules to footer component

--- front/src/app/app.module.ts
+++ front/src/app/app.module.ts
@@ -5,11 +5,15 @@ import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
 import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
 import { HeaderComponent } from './header/header.component';
+import { FooterComponent } from './footer/footer.component';
+import { MainComponent } from './main/main.component';
 
 @NgModule({
   declarations: [
     AppComponent,
-    HeaderComponent
+    HeaderComponent,
+    FooterComponent,
+    MainComponent
   ],
   imports: [
     BrowserModule,
